f01e7415b9e28759a4b865ca2e15024d3cd3d629
jsnbrs <jasonbooras@gmail.com>
2016-10-03 17:31:49 -0400

    add flexbox and hover info on imgs

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,58 +1,62 @@
 $(function() {
   "use strict";
 
-    $(document).ready(function() {
-      $('body').scrollspy({target: ".navbar", offset: 50});
-      $("#navbar a").on('click', function(event) {
-        event.preventDefault();
+  $('body').scrollspy({target: ".navbar", offset: 50});
 
-        // Store hash
-        var hash = this.hash;
-        // Store navbar height
-        var startY = $(".navbar").height() * 2;
+  $("#navbar a").on('click', function(event) {
+    event.preventDefault();
 
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function() {
+    // Store hash
+    var hash = this.hash;
+    // Store navbar height
+    var startY = $(".navbar").height() * 2;
 
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      });
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function() {
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
     });
-    //above code is from http://www.w3schools.com/bootstrap/bootstrap_ref_js_scrollspy.asp
+  });
+  //above smooth scrolling is from http://www.w3schools.com/bootstrap/bootstrap_ref_js_scrollspy.asp
 
-// code below from stackoverflow: http://stackoverflow.com/questions/29646622/set-bootstrap-navbar-transparency-on-scroll
 
-    function checkScroll() {
-        var startY = $('.navbar').height() * 2;
+  function checkScroll() {
+    var startY = $('.navbar').height() * 2;
 
-        if($(window).scrollTop() > startY){
-            $('.navbar').addClass("scrolled");
-        } else {
-            $('.navbar').removeClass("scrolled");
-        }
+    if($(window).scrollTop() > startY){
+      $('.navbar').addClass("scrolled");
+    } else {
+      $('.navbar').removeClass("scrolled");
     }
+  }
 
-    if($('.navbar').length > 0) {
-        $(window).on("scroll load resize", function(){
-            checkScroll();
-        });
+  if($('.navbar').length > 0) {
+    $(window).on("scroll load resize", function(){
+      checkScroll();
+      });
     }
-});
+  //above navbar transparency on scroll from stackoverflow: http://stackoverflow.com/questions/29646622/set-bootstrap-navbar-transparency-on-scroll
 
-// code below is from http://stackoverflow.com/questions/17507870/jquery-image-changing-on-hover
-// jQuery to change image on hover for avatar
 
-    var sourceSwap = function() {
+  $('img.img-circle').hover(sourceSwap, sourceSwap);
+
+  function sourceSwap() {
     var $this = $(this);
     var newSource = $this.data('alt-src');
+
     $this.data('alt-src', $this.attr('src'));
     $this.attr('src', newSource);
-}
+  }
 
-$(function () {
-    $('img.img-circle').hover(sourceSwap, sourceSwap);
+
+  $('.gallery-item').hover(function () {
+      $(this).find('.img-title').fadeIn(300);
+  }, function() {
+      $(this).find('.img-title').fadeOut(200);
+  });
+
 });
